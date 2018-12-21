f3bab25bfec30086b4e208f63ee82c00e1d1a312
GuofengBai <GuofengBai@yeah.net>
2018-12-21 23:03:34 +0800

    initiate projects.

---
+++ src/test/java/com/nju/edu/cn/whispermusic/WhisperMusicApplicationTests.java
@@ -0,0 +1,17 @@
+package com.nju.edu.cn.whispermusic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhisperMusicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
