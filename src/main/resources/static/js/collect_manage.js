8b9097f63ccf2d4566df82ac76fec471f4e8ae14
chiyo <cinro9@gmail.com>
2019-01-08 14:39:07 +0800

    manage

---
+++ src/main/resources/static/js/collect_manage.js
@@ -0,0 +1,22 @@
+function delCollect(id){
+    if(!(id)) {
+        alert('请输入全部字段！');
+        return;
+    }
+    $.ajax({
+        url : '/whisper/'+id+'/delete',
+        data : {
+        },
+        dataType : 'json',
+        type : 'post',
+        success: function (result) {
+            location.reload(true);
+            alert('操作成功！');
+        },
+        error:function(err){
+            location.reload(true);
+            alert('操作失败！');
+        }
+    });
+
+}
