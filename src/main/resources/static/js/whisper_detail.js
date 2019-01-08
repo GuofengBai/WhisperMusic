506f21d03aa8eb216a4a3b42182de13319ac867b
InterstellarNolan <georgecurz@qq.com>
2019-01-08 21:50:59 +0800

    Like and Favorite

--- src/main/resources/static/js/whisper_detail.js
+++ src/main/resources/static/js/whisper_detail.js
@@ -1,4 +1,26 @@
-function Ilike(id){
+function replylike(id){
+    if(!(id)) {
+        alert('请输入全部字段！');
+        return;
+    }
+    $.ajax({
+        url : '/reply/'+id+'/like',
+        data : {
+        },
+        dataType : 'json',
+        type : 'post',
+        success: function (result) {
+            location.reload(true);
+            alert('赞成功！');
+        },
+        error:function(err){
+            location.reload(true);
+            alert('赞失败！');
+        }
+    });
+
+}
+function whisperlike(id){
     if(!(id)) {
         alert('请输入全部字段！');
         return;
