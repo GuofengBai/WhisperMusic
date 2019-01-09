89d34f7d8c6f4df13fedbe53f0c11574b5bcf9c5
chiyo <cinro9@gmail.com>
2019-01-09 18:44:07 +0800

    stick reply

--- src/main/resources/static/js/reply_manage.js
+++ src/main/resources/static/js/reply_manage.js
@@ -21,13 +21,13 @@ function delReply(id){
 
 }
 
-function upReply(id){
+function stickReply(id){
     if(!(id)) {
         alert('请输入全部字段！');
         return;
     }
     $.ajax({
-        url : '/reply/'+id+'/delete',
+        url : '/reply/'+id+'/stick',
         data : {
         },
         dataType : 'json',
@@ -35,6 +35,29 @@ function upReply(id){
         success: function (result) {
             location.reload(true);
             alert('置顶成功！');
+        },
+        error:function(err){
+            location.reload(true);
+            alert('操作失败！');
+        }
+    });
+
+}
+
+function unstickReply(id){
+    if(!(id)) {
+        alert('请输入全部字段！');
+        return;
+    }
+    $.ajax({
+        url : '/reply/'+id+'/unstick',
+        data : {
+        },
+        dataType : 'json',
+        type : 'post',
+        success: function (result) {
+            location.reload(true);
+            alert('操作成功！');
         },
         error:function(err){
             location.reload(true);
