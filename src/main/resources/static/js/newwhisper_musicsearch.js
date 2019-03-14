b16da15b0d40c48ab4bbe8c3d4e8c44da4eaf91a
InterstellarNolan <georgecurz@qq.com>
2019-03-14 22:42:56 +0800

    搜索音乐并加到悄悄话

--- src/main/resources/static/js/newwhisper_musicsearch.js
+++ src/main/resources/static/js/newwhisper_musicsearch.js
@@ -14,9 +14,10 @@ function searchMusic() {
                 '            </thead>';
             var songs = result.result.songs;
             for (var i in songs) {
-                if (songs[i].fee === 0){
+                if (songs[i].fee === 0) {
                     var str = '<td>' + songs[i].name + ' - ' + songs[i].artists[0].name + '</td>';
-                    str = str + '<td>' + '<button class=\"btn btn-primary\" type=\"button\" onclick=\"javascript:musicChange(' + songs[i].id + ')\">播放</button>' + '</td>';
+                    str = str + '<td>' + '<button class=\"btn btn-primary\" id=\"playMusic\" type=\"button\" onclick=\"javascript:musicChange(' + songs[i].id + ')\">播放</button>' + '</td>';
+                    str = str + '<td>' + '<button class=\"btn btn-block\" id="addMusic" type=\"button\" onclick=\"javascript:musicAdd(' + songs[i].id + ')\">加入到悄悄话</button>' + '</td>';
                     str = '<tr>' + str + '</tr>';
                     $("#songtable").append(str)
                 }
@@ -34,7 +35,10 @@ function searchMusic() {
 function musicChange(id) {
     var url = "//music.163.com/outchain/player?type=2&id=" + id + "&auto=1&&height=66";
     document.getElementById('music_player').src = url;
-    document.getElementById('musicInfo').content=id;
+}
 
-
+function musicAdd(id, name) {
+    document.getElementById('hasMusic').setAttribute('value', 'true');
+    document.getElementById('musicId').setAttribute('value', id);
+    document.getElementById('musicName').setAttribute('value', name);
 }
