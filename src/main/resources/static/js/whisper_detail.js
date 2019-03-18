733953d065634c6d15a766e3223c278ce443af5b
chiyo <cinro9@gmail.com>
2019-03-18 22:46:45 +0800

    ...

--- src/main/resources/static/js/whisper_detail.js
+++ src/main/resources/static/js/whisper_detail.js
@@ -86,3 +86,48 @@ function unfavourite(id){
     });
 
 }
+
+
+function favouriteMusic(id,name){
+    if(!(id)) {
+        alert('请输入全部字段！');
+        return;
+    }
+    $.ajax({
+        url : '/music/'+id+'/favorite?musicName='+name,
+        data : {
+        },
+        dataType : 'json',
+        type : 'post',
+        success: function (result) {
+            alert('收藏成功！');
+
+        },
+        error:function(err){
+            location.reload(true);
+            alert('收藏失败！');
+        }
+    });
+
+}
+
+function unfavouriteMusic(id) {
+    if (!(id)) {
+        alert('请输入全部字段！');
+        return;
+    }
+    $.ajax({
+        url: '/music/' + id + '/unfavorite',
+        data: {},
+        dataType: 'json',
+        type: 'post',
+        success: function (result) {
+            location.reload(true);
+
+        },
+        error: function (err) {
+            location.reload(true);
+            alert('取消收藏失败！');
+        }
+    });
+}
